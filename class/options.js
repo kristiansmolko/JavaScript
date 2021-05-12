ce50160fae5697e2619e30c3d1402db9545c7b5c
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-05-12 09:50:27 +0200

    Added options config

---
+++ class/options.js
@@ -0,0 +1,33 @@
+const saveOptions = function () {
+    const weatherKey = document.getElementById('weatherKey').value
+    const geoKey = document.getElementById('geoKey').value
+
+    chrome.storage.sync.set({
+        weatherAPIKey: weatherKey,
+        geoAPIKey: geoKey
+    }, () => {
+        // Update status to let user know options were saved.
+        const status = document.getElementById('status');
+
+        status.textContent = 'Options saved.';
+        setTimeout(function () {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+const restoreOptions = function () {
+    chrome.storage.sync.get(
+        ['weatherAPIKey', 'geoAPIKey']
+    , (items) => {
+        if(items.weatherAPIKey){
+            document.getElementById('weatherKey').value = items.weatherAPIKey;
+        }
+        if(items.geoAPIKey){
+            document.getElementById('geoKey').value = items.geoAPIKey;
+        }        
+    });
+};
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('submit').addEventListener('click', saveOptions);
